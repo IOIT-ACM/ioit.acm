9144c20b2e40b28063297e4a461ae420f7975c53
Aditya <adimail2404@gmail.com>
2024-11-28 17:54:55 +0530

    switched to static image paths for teams

--- app/blueprints/team.py
+++ app/blueprints/team.py
@@ -11,11 +11,6 @@ def team():
     latest_year = years[0]  # Get the latest year
     selected_team_data = team_data.get(latest_year, [])
 
-    # Add URLs for static image paths
-    for member in selected_team_data:
-        member.setdefault("socials", {})
-        member["image_url"] = url_for("static", filename="img/team/" + member["image"])
-
     return render_template(
         "team.html",
         years=years,
@@ -35,37 +30,37 @@ def fetch_team():
         # Generate social media links
         social_links = ""
 
-        if member.get("socials", {}).get("github"):
+        if member.get("github"):
             social_links += """
             <a href="{github_url}" target="_blank"
                 class="text-gray-600 hover:text-gray-400 transition duration-200">
                 <i class="fab fa-github text-base"></i>
             </a>
-            """.format(github_url=member["socials"]["github"])
+            """.format(github_url=member["github"])
 
-        if member.get("socials", {}).get("linkedin"):
+        if member.get("linkedin"):
             social_links += """
             <a href="{linkedin_url}" target="_blank"
                 class="text-blue-600 hover:text-blue-400 transition duration-200">
                 <i class="fab fa-linkedin text-base"></i>
             </a>
-            """.format(linkedin_url=member["socials"]["linkedin"])
+            """.format(linkedin_url=member["linkedin"])
 
-        if member.get("socials", {}).get("instagram"):
+        if member.get("instagram"):
             social_links += """
             <a href="{instagram_url}" target="_blank"
                 class="text-pink-600 hover:text-pink-400 transition duration-200">
                 <i class="fab fa-instagram text-base"></i>
             </a>
-            """.format(instagram_url=member["socials"]["instagram"])
+            """.format(instagram_url=member["instagram"])
 
-        if member.get("socials", {}).get("linktree"):
+        if member.get("linktree"):
             social_links += """
             <a href="{linktree_url}" target="_blank"
                 class="text-green-600 hover:text-green-400 transition duration-200">
                 <i class="fas fa-link text-base"></i>
             </a>
-            """.format(linktree_url=member["socials"]["linktree"])
+            """.format(linktree_url=member["linktree"])
 
         # Render member card
         rendered_cards += """
@@ -77,7 +72,7 @@ def fetch_team():
             class="w-44 h-44 md:w-48 md:h-48 bg-gray-100 border border-gray-300 overflow-hidden rounded-full mb-4 shadow-lg"
           >
             <img
-              src="{image_url}"
+              src="{image}"
               alt="{name}"
               class="w-full h-full object-cover object-top transition-transform duration-300 hover:scale-110"
             />
@@ -92,7 +87,7 @@ def fetch_team():
           </div>
         </div>
         """.format(
-            image_url=url_for("static", filename="img/team/" + member["image"]),
+            image=member["image"],
             name=member["name"],
             title=member["title"],
             social_links=social_links,
