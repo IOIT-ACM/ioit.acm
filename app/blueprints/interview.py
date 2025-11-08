c20b0ddaae01ad204fa499921200f734fde74000
Aditya <adimail2404@gmail.com>
2025-11-08 23:34:16 +0530

    feat: acm phase 3 interviews

--- app/blueprints/interview.py
+++ app/blueprints/interview.py
@@ -7,3 +7,8 @@ interviews_bp = Blueprint("interviews", __name__, template_folder="../templates"
 @interviews_bp.route("/interviews/tenet-p1")
 def home():
     return render_template("interviews/tenet.phase1.html")
+
+
+@interviews_bp.route("/interviews/acmp3")
+def acm_recruitment_p3():
+    return render_template("interviews/acm.25.p3.html")
